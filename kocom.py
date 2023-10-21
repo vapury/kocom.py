f268cd5ba2cb6ea87c73694c3b42667c9208d969
vapury <41498409+vapury@users.noreply.github.com>
2023-10-21 11:20:05 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -645,12 +645,14 @@ def packet_processor(p):
             logtxt = '[MQTT publish|gas] data[{}]'.format(state)
             mqttc.publish('kocom/livingroom/gas/state', json.dumps(state), retain=True)
     elif p['type'] == 'send' and p['dest'] == 'elevator':
-        floor = int(p['value'][2:4], 16)
+       # floor = int(p['value'][2:4], 16)
         rs485_floor = int(config.get('Elevator', 'rs485_floor', fallback=0))
         if rs485_floor != 0:
-            state = {'floor': floor}
-            if rs485_floor == floor:
-                state['state'] = 'off'
+         if ['value'} == '0300000000000000' :
+           # state = {'floor': floor}
+             state = {'state': 'off'}
+           # if rs485_floor == floor:
+               # state['state'] = 'off'
         else:
             state = {'state': 'off'}
         logtxt = '[MQTT publish|elevator] data[{}]'.format(state)
